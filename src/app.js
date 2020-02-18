8b4362852f867018b188d2e64f06ef057dce9c50
Nothile Ngcobo <nothilengcobo@nothiles-MacBook-Pro.local>
2020-02-18 15:36:24 +0200

    added pug

--- src/app.js
+++ src/app.js
@@ -1,49 +1,54 @@
 const express = require('express')
 const app = express()
-
-
-
+const path = require('path');
 
 app.use(express.urlencoded());
 app.use('/', express.static(__dirname + '/'));
 app.set('view engine', 'pug')   
 
-
-
-
-//rendering the form
-app.get('/', (req, res) => res.sendFile('${_dirname} /index'))
-app.get('/newVisitor', function(req, res){
-
-
+//rendering the form to host 
+app.get('/new_Visitor', (req, res) =>{ res.sendFile('${_dirname} /index.html')})
+app.get('/', function(req, res){
     res.render('newVisitor');
  });
 
- // post which sends to database
- app.post('/person', function(req, res){
-    var personInfo = req.body; //Get the parsed information
+
+// creating the table
+ const createTable = async () => {
+     try{
+        const query = await pool.query(
+           `CREATE TABLE IF NOT EXISTS
+            VISITORS(
+            ID  SERIAL PRIMARY KEY,
+            visitor_name VARCHAR(60),
+            visitors_age  INT,
+            date_of_visit  DATE,
+            time_of_visit  TIME,
+            assistant_name VARCHAR(60),
+            comments        VARCHAR(200)
+
+           );`
+       )
+        console.log(query)
+        console.log('table created successful')
+
+     }catch(e) {
+         console.log(e);
+
+     };
+};
+
+ //post which sends to database
+ app.post('/action', function(req, res){
+     console.log(JSON.stringify(req.body));
+
+     var visitor = req.body['visitor_name'];
+     var assistant_name = req.body['assistant_name'];
+     var visitors_age = req.body['visitor_age'];
+     var date_of_visit =req.body['date_of_visit'];
+     var comments =req.body['comments'];
+ }
     
-    if(!personInfo.name || !personInfo.age || !personInfo.nationality){
-       res.render('show_message', {
-          message: "Sorry, you provided worng info", type: "error"});
-    } else {
-       var newPerson = new Person({
-          name: personInfo.name,
-          age: personInfo.age,
-          nationality: personInfo.nationality
-       });
-         
-       newPerson.save(function(err, Person){
-          if(err)
-             res.render('show_message', {message: "Database error", type: "error"});
-          else
-             res.render('show_message', {
-                message: "New person added", type: "success", person: personInfo});
-       });
-    }
- });
 
-
-
-app.listen(3000, () => console.log(` app listening on port ${3000}!`))
+ app.listen(3000, () => console.log(` app listening on port ${3000}!`));
 
