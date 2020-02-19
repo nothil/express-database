13c4ae6b880e161150ff0050dfe23ae823c19e8d
nothile <nothile.ngcobo@umuzi.org>
2020-02-19 13:01:40 +0200

    added pug

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+const {addNewVisitor,createTable} = require('./database')
 const express = require('express')
 const app = express()
 const path = require('path');
@@ -7,79 +8,32 @@ app.use('/', express.static(__dirname + '/'));
 app.set('view engine', 'pug')  
 
 
-const Pool  = require('pg').Pool;
-
-const pool = new Pool({
-    user: 'user' ,
-    host:'localhost',
-    database: 'db',
-    password: 'pass',
-    port: 8080,
-})
-
-
 //rendering the form to host 
-app.get('/new_Visitor', (req, res) =>{ res.sendFile('${_dirname} /index.html')})
-app.get('/', function(req, res){
-    res.render('newVisitor');
- })
+app.get('/new_Visitor', (req, res) =>{ 
+    res.sendFile('${_dirname} /index.html')})
 
 
 
 // creating the table
- const createTable = async () => {
-     try{
-        const query = await pool.query(
-           `CREATE TABLE IF NOT EXISTS
-            VISITORS(
-            ID  SERIAL PRIMARY KEY,
-            visitor_name VARCHAR(60),
-            visitors_age  INT,
-            date_of_visit  DATE,
-            time_of_visit  TIME,
-            assistant_name VARCHAR(60),
-            comments        VARCHAR(200)
-
-           );`
-       )
-        console.log(query)
-        console.log('table created successful')
-
-     }catch(e) {
-         console.log(e);
-
-     }
-}
-
-
-const addNewVisitor = async (name, age, date, time, assistant, comment) => {
-    try{
-       const query = await pool.query(
-          'INSERT INTO VISITORS(visitor_name,visitors_age,date_of_visit,time_of_visit,assistant_name,comments) VALUES ($1,$2,$3,$4,$5,$6)',
-            [name,age,date,time,assistant,comment]       
-      )
-       console.log(query.rows)
-       console.log('data saved')
-       
-    }catch(e) {
-        console.log(e);
-
-    };
-};
+ 
+createTable();
 
 // post which sends to database
  app.post('/action', function(req, res){
-     console.log(JSON.stringify(req.body));
-
-     var visitor = req.body['visitor_name'];
-     var assistant_name = req.body['assistant_name'];
-     var visitors_age = req.body['visitor_age'];
-     var time_of_visit = req.body['time_of_visit'];
-     var date_of_visit =req.body['date_of_visit'];
-     var comments =req.body['comments'];
-     addNewVisitor(visitor_name, assistant_name, visitors_age, date_of_visit, time_of_visit);
+     //console.log(JSON.stringify(req.body));
+     const visitor = req.body.name;
+     const assistant_name = req.body.assistant;
+     const visitors_age = req.body.age;
+     const time_of_visit = req.body.time;
+     const date_of_visit =req.body.date;
+     const comments =req.body.comment;
+     console.log(visitor);
+     addNewVisitor(visitor, visitors_age, date_of_visit, time_of_visit, assistant_name,comments);
+     res.render('newVisitor')
+     res.end();
  });
     
 
- app.listen(3000, () => console.log(` app listening on port ${3000}!`));
+
+app.listen(3000, () => console.log(` app listening on port ${3000}!`));
 
